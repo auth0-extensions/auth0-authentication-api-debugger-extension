e811e7db30a172f07b18f4b8d6ffa7d368e873cd
Jerrie Pelser <jerrie@jerriepelser.com>
2016-11-08 14:23:54 +0700

    Generate bundle

--- build/bundle.js
+++ build/bundle.js
@@ -47,26 +47,30 @@ module.exports =
 
 	'use strict';
 
-	var App = __webpack_require__(1);
+	var Webtask = __webpack_require__(1);
 
-	var port = process.env.PORT || 3000;
-
-	App.listen(port, function () {
-	    console.log('Server started on port', port);
-	});
+	// This is the entry-point for the Webpack build. We need to convert our module
+	// (which is a simple Express server) into a Webtask-compatible function.
+	module.exports = Webtask.fromExpress(__webpack_require__(2));
 
 /***/ },
 /* 1 */
+/***/ function(module, exports) {
+
+	module.exports = require("webtask-tools");
+
+/***/ },
+/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	var path = __webpack_require__(2);
-	var crypto = __webpack_require__(3);
-	var express = __webpack_require__(4);
-	var bodyParser = __webpack_require__(5);
-	var handlebars = __webpack_require__(6);
-	var Webtask = __webpack_require__(7);
+	var path = __webpack_require__(3);
+	var crypto = __webpack_require__(4);
+	var express = __webpack_require__(5);
+	var bodyParser = __webpack_require__(6);
+	var handlebars = __webpack_require__(7);
+	var Webtask = __webpack_require__(1);
 	var expressTools = __webpack_require__(8);
 	var metadata = __webpack_require__(49);
 
@@ -138,40 +142,34 @@ module.exports =
 	module.exports = app; //Webtask.fromExpress(app);
 
 /***/ },
-/* 2 */
+/* 3 */
 /***/ function(module, exports) {
 
 	module.exports = require("path");
 
 /***/ },
-/* 3 */
+/* 4 */
 /***/ function(module, exports) {
 
 	module.exports = require("crypto");
 
 /***/ },
-/* 4 */
+/* 5 */
 /***/ function(module, exports) {
 
 	module.exports = require("express");
 
 /***/ },
-/* 5 */
+/* 6 */
 /***/ function(module, exports) {
 
 	module.exports = require("body-parser");
 
 /***/ },
-/* 6 */
-/***/ function(module, exports) {
-
-	module.exports = require("handlebars");
-
-/***/ },
 /* 7 */
 /***/ function(module, exports) {
 
-	module.exports = require("webtask-tools");
+	module.exports = require("handlebars");
 
 /***/ },
 /* 8 */
@@ -202,7 +200,7 @@ module.exports =
 /***/ function(module, exports, __webpack_require__) {
 
 	const tools = __webpack_require__(10);
-	const Webtask = __webpack_require__(7);
+	const Webtask = __webpack_require__(1);
 
 	module.exports.createServer = function(cb) {
 	  return Webtask.fromExpress(tools.createServer(cb));
@@ -213,7 +211,7 @@ module.exports =
 /* 10 */
 /***/ function(module, exports, __webpack_require__) {
 
-	const Webtask = __webpack_require__(7);
+	const Webtask = __webpack_require__(1);
 
 	const errors = __webpack_require__(11);
 	const storage = __webpack_require__(19);
